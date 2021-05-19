000bd55458d457feb81e37c9a5ce38451cfe87d1
djmontes <MONTES.DARYLJAY@UE.EDU.PH>
2021-05-19 16:04:48 +0800

    Almost Finished About Us

---
+++ app/src/main/java/com/example/finalsproject/HowToPlay.java
@@ -0,0 +1,14 @@
+package com.example.finalsproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HowToPlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.HowToPlay);
+    }
+}
